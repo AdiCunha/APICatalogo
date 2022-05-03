b9e75fbbb114d965d57a5b6d82d9e9e08032733d
Adí Santos <adi.santos@sequor.com.br>
2022-05-03 15:18:38 -0300

    Seção 3 - aula 44.

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
@@ -51,5 +52,47 @@ namespace APICatalogo.Controllers
             return new CreatedAtRouteResult("ObterProduto",
                 new { id = produto.ProdutoId }, produto);
         }
+
+        [HttpPut]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p =>p.ProdutoId == id);
+            //var produto = _context.Produtos.Find(id);
+
+
+            if (produto is null)
+            {
+                return NotFound("Produto não localizado.");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+
+
+
+
+
+
+        }
+
+
+
+
     }
 }
