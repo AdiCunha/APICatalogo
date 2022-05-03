de12d14e1cb5fa41fb77cbde50a2706673b76ea5
Adí Santos <adi.santos@sequor.com.br>
2022-05-03 11:36:05 -0300

    Seção 3 - Aula 34.

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
         public Categoria()
@@ -9,8 +12,15 @@ namespace APICatalogo.Models
             Produtos = new Collection<Produto>();
         }
 
+        [Key]
         public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage= "Tamanho do texto inválido. O valor máximo possível é 80 caracteres.")]
+        [MaxLength(80)]
         public string? Nome { get; set; }
+
+        [Required]
+        [MaxLength(300)]
         public string? ImagemUrl { get; set; }
 
         public ICollection<Produto> Produtos { get; set; }
