4968be53704dd4410d66879341bfd565c1c4046b
Adí Santos <adi.santos@sequor.com.br>
2022-05-03 13:11:40 -0300

    Seção 3 - Aula 36.

---
+++ APICatalogo/Migrations/20220503145303_PopulaCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Categorias(Nome, ImagemURL) values ('Bebidas','bebodas.jpg')");
+            mb.Sql("INSERT INTO Categorias(Nome, ImagemURL) values ('Lanches','lanches.jpg')");
+            mb.Sql("INSERT INTO Categorias(Nome, ImagemURL) values ('Sobremesas','sobremesas.jpg')");
+
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+
+            mb.Sql("DELETE FROM Categorias");
+                    }
+    }
+}
